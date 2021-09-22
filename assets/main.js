a715df6beea9f3cf2e4eb0ccff5801bb83f76d36
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2021-09-22 17:28:31 -0400

    added variables with id attached to each section of the body in html file, delted header tags inside sections, added button in js

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,18 @@
 console.log("I'm connected");
 
+var questionContainer = document.getElementById("question");
+var answerContainer = document.getElementById("answer");
+var resultContainer = document.getElementById("result");
+var timerContainer = document.getElementById("timer");
+var highScoreContainer = document.getElementById("highScore");
+
 var buttonTag = document.createElement("button");
-buttonTag.setAttribute("type", "test image")
 document.body.appendChild(buttonTag);
 
+
+buttonTag.addEventListener("click", function(){
+    console.log("Start of quiz")
+});
 
 
 // initial screen
@@ -15,11 +24,12 @@ document.body.appendChild(buttonTag);
 
 // after start quiz is selcted
 // - time = however long for quiz (top right)
-// - <h2>question</h2> 
+// - <h1>question</h1> 
 // - <form> <input type="radio" id="1" name="first-qustion" value="first answer">
-//   <form> <input type="radio" id="2" name="first-qustion" value="second answer">
-//   <form> <input type="radio" id="3" name="first-qustion" value="third answer">
-//   <form> <input type="radio" id="4" name="first-qustion" value="fourth answer">
+//           <input type="radio" id="2" name="first-qustion" value="second answer">
+//           <input type="radio" id="3" name="first-qustion" value="third answer">
+//           <input type="radio" id="4" name="first-qustion" value="fourth answer">
+// - </form>
 
 // - if/else statements to determine if correct or wrong
 // - a way to subtract certain amount of time when wrong
@@ -28,14 +38,14 @@ document.body.appendChild(buttonTag);
 
 // after questions answered
 // - timer stops after last question answered
-// - <h2> "finished"</h2>
+// - <h1> "finished"</h1>
 // - <p> "your score is" </p>
 // - <form id=idforform action=do we need one?> "your initials" : <input type="text" name= "idk yet">
 //   <input type="button" onclick="a function name" value="submit"> </form>
 
 // once initials submitted
 // - timer and view highscores no longer visible at top of screen
-// <h2> Highscores </h2>
+// <h1> Highscores </h1>
 // <ol><li> value from initials and score</li></ol>
 // -I'll need a way to add items to <ol> 
 // -2 buttons for "go back" and "clear highscores"
